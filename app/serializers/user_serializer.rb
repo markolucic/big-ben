20df44b01d53bddf6f50b02394b2652edd5a95b3
markolucic <marko_lucic_1994@yahoo.com>
2017-03-13 10:05:03 -0700

    user serializer error fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
-class CourseSerializer < ActiveModel::Serializer
+class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :nickname, :image, :email, :tokens, :encrypted_password, :uid
 end
