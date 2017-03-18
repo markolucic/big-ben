f29cf39bbafa0f1d064103da848421b77dfe7b74
markolucic <marko_lucic_1994@yahoo.com>
2017-03-18 04:22:36 -0700

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all#.where(role == student)
+		render json: @users
+	end
+
   def show
 		@user = User.find_by(id: params[:id])
 		render json: @user
