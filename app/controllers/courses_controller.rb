0d24c85f2b34bff813a75d8641b28bc5a6aaa775
markolucic <marko_lucic_1994@yahoo.com>
2017-03-27 12:23:31 -0700

    finished course and students controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
 	before_action :authenticate_user, only: [:create, :edit, :update, :destroy]
+	before_action :set_course
 
 	def index
 		@courses = Course.all
@@ -7,7 +8,37 @@ class CoursesController < ApplicationController
 	end
 
 	def show
-		@course = Course.find_by(id: params[:id])
 		render json: @course
+	end
+
+	def create
+		@course = Course.create(course_params)
+		if @course.save
+			render json: @course, status: :created
+		else
+			render json: @course.errors, status: :unprocessable_entity 
+		end
+	end
+
+	def update
+		if @course.update_attributes(course_params)
+			render json: @course, status: 200
+		else
+			render json: {}, status: 500
+		end
+	end
+
+	def destroy
+		@course.destroy
+		render json: {}, status: :no_content
+	end
+
+	private
+	def set_course
+		@course = Course.find_by(id: params[:id])
+	end
+
+	def course_params
+		params.require(:course).permit(:name, :description, :image, :price)
 	end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
 	before_action :authenticate_user, only: [:create, :edit, :update, :destroy]
+	before_action :set_student
 
 	def index
 		@students = Student.all
@@ -7,7 +8,38 @@ class StudentsController < ApplicationController
 	end
 
 	def show
-		@student = Student.find_by(id: params[:id])
 		render json: @student
+	end
+
+	def create
+		@student = Student.create(student_params)
+		if @student.save
+			render json: @student, status: :created
+		else
+			render json: @student.errors, status: :unprocessable_entity 
+		end
+	end
+
+	def update
+		if @student.update_attributes(student_params)
+			render json: @student, status: 200
+		else
+			render json: {}, status: 500
+		end
+	end
+
+	def destroy
+		@student.destroy
+		render json: {}, status: :no_content
+	end
+
+	private
+
+	def set_student
+		@student = Student.find_by(id: params[:id])
+	end
+
+	def student_params
+		params.require(:student).permit(:name, :address, :phone)
 	end
 end
