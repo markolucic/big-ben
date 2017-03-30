8c72d9c0d057398f04a16ceb3c02fe3ba06abd89
markolucic <marko_lucic_1994@yahoo.com>
2017-03-30 12:00:18 -0700

    add months controller

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -1,6 +1,44 @@
 class MonthsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_month
+
 	def index
 		@months = Month.all
 		render json: @months
+	end
+
+	def show
+		render json: @month
+	end
+
+	def create
+		@month = Month.create(month_params)
+		if @month.save
+			render json: @month, status: :created
+		else
+			render json: @month.errors, status: :unprocessable_entity 
+		end
+	end
+
+	def update
+		if @month.update_attributes(month_params)
+			render json: @month, status: 200
+		else
+			render json: {}, status: 500
+		end
+	end
+
+	def destroy
+		@month.destroy
+		render json: {}, status: :no_content
+	end
+
+	private
+	def set_month
+		@month = Month.find_by(id: params[:id])
+	end
+
+	def month_params
+		params.require(:month).permit(:name)
 	end
 end
