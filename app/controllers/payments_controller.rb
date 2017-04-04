ac2e455c9fa5dadb258201ca3fa0870c6d4a0a2d
markolucic <marko_lucic_1994@yahoo.com>
2017-04-04 04:20:28 -0700

    payment course

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,7 @@ class PaymentsController < ApplicationController
 
 	def index
 		@payments = Payment.all
-		render json: @payments#, include: 'month'
+		render json: @payments, include: 'course'
 	end
 
 	def show
