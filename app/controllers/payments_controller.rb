a5e7f38b9417e1deb39b799d3736601e4a4e0782
markolucic <marko_lucic_1994@yahoo.com>
2017-04-04 04:40:26 -0700

    json fix

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,7 @@ class PaymentsController < ApplicationController
 
 	def index
 		@payments = Payment.all
-		render json: @payments, include: 'student.course', include: 'month'
+		render json: @payments, include: {'student.course', 'month'}
 	end
 
 	def show
